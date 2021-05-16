36b2f0cf02881ac6a0ec9eec961468867040b72a
d2jvkpn <chenbin2018@protonmail.com>
2021-05-16 21:36:53 +0800

    ...

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,6 @@ pip3 install yq
 yq -r ".services | keys" docker-compose.yaml
 yq -r ".services | keys[]" docker-compose.yaml
 
-
 #### get ip address of mysql_service
 docker inspect mysql_service | jq -r ".[0].NetworkSettings.IPAddress"
 mysql -u root -h ${IP} -p
