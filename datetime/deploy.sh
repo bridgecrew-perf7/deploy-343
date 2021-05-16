0529d287db4f64fae1eb420872864ee1627a9eeb
d2jvkpn <chenbin2018@protonmail.com>
2021-05-16 21:35:43 +0800

    add datetime and shiori

---
+++ datetime/deploy.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+#### config
+PORT="$1"
+image="registry.cn-shanghai.aliyuncs.com/d2jvkpn/datetime:latest"
+
+#### deploy
+docker pull $image
+export PORT=${PORT}
+envsubst < deployment.yaml > docker-compose.yaml
+docker-compose up -d
+
+echo ">>> HTTP server port: $PORT"
+docker logs datetime_service
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,27 @@
 #! /usr/bin/env bash
 set -eu -o pipefail
 
-docker-compose --env-file ./docker-env up -d
+## pull lastest images
+yq -r ".services | .[] | .image" docker-compose.yaml |
+    awk '$1!~":local$"' | xargs -i docker pull {}
 
-# docker inspect mysql_service | jq -r ".[0].NetworkSettings.IPAddress"172.17.0.2
+if [ -f "./docker.env" ]; then
+    docker-compose --env-file ./docker.env up -d
+else
+    docker-compose up -d
+fi
+
+
+exit
+
+#### install yq
+pip3 install yq
+
+#### list services
+yq -r ".services | keys" docker-compose.yaml
+yq -r ".services | keys[]" docker-compose.yaml
+
+
+#### get ip address of mysql_service
+docker inspect mysql_service | jq -r ".[0].NetworkSettings.IPAddress"
+mysql -u root -h ${IP} -p
