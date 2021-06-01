99d7ca19306b7aa4bb918611fbc66974eb297299
d2jvkpn <chenbin2018@protonmail.com>
2021-06-01 09:26:35 +0800

    ...

--- registry/do.sh
+++ registry/do.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 set -eu -o pipefail
 
-curl -X GET 127.0.0.1:5000/v2/image_name/tags/list
 
 mkdir -p auth data
 
@@ -10,18 +9,22 @@ apt install apache2-utils
 # htpasswd -Bbn USERNAME >> auth/htpasswd
 htpasswd -Bc auth/htpasswd USERNAME
 
-#### exit
+exit
+
+DOMAIN=xxxx
+
 docker pull alpine:3
-docker tag alpine:3 localhost:5000/alpine:3
-docker push localhost:5000/alpine:3
+docker tag alpine:3 $DOMAIN/alpine:3
+docker push $DOMAIN/alpine:3
+
+docker login ${DOMAIN}
 
 # append /etc/docker/daemon.json.{.registry-mirrors}    http://localhost:5000
 # append /etc/docker/daemon.json.{.insecure-registries} http://localhost:5000
 
-# docker login localhost:5000
-
-# curl localhost:5000/v2/_catalog
-# curl localhost:5000/v2/alpine/tags/list
+# https://$DOMAIN/v2/alpine/tags/list
+# https://$DOMAIN/v2/_catalog
+# https://$DOMAIN/v2/alpine/tags/list
 
 docker run -d \
   -p 5000:5000 \
