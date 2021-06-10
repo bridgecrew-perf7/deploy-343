2aa4cec325a39ab21d810f3d44aca18c38d5712d
d2jvkpn <chenbin2018@protonmail.com>
2021-06-10 19:04:50 +0800

    ...

--- yapi/deploy_yapi.sh
+++ yapi/deploy_yapi.sh
@@ -30,13 +30,25 @@ docker run -it --rm       \
 
 ####
 docker run --detach           \
-  --name=yapi                 \
+  --name=yapi_service         \
   --restart=always            \
   --link yapi_mongo:mongo     \
   --publish=${HTTP_Port}:3000 \
   --workdir /api/vendors      \
   registry.cn-hangzhou.aliyuncs.com/anoy/yapi \
   server/app.js
-
 # default account: admin@admin.com
 # default password: ymfe.org
+
+docker cp yapi:/api api
+docker rm -f yapi_service
+
+docker run --detach           \
+  --name=yapi_service         \
+  --restart=always            \
+  --link yapi_mongo:mongo     \
+  --publish=${HTTP_Port}:3000 \
+  --volume=$PWD/api:/api      \
+  --workdir=/api/vendors      \
+  registry.cn-hangzhou.aliyuncs.com/anoy/yapi \
+  server/app.js
