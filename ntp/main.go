8a3559e57a4eec2685bcc2ec93147f7bb308621c
d2jvkpn <chenbin2018@protonmail.com>
2021-08-20 00:16:53 +0800

    ...

--- ntp/main.go
+++ ntp/main.go
@@ -9,17 +9,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	_App = "ntp"
+)
+
 func init() {
 	if os.Getenv("TZ") == "" {
 		os.Setenv("TZ", "Asia/Shanghai")
 	}
 
-	os.Setenv("APP_Name", "ntp")
+	os.Setenv("APP_Name", _App)
 	misc.SetLogRFC3339()
 }
 
 func main() {
-	rootCmd := &cobra.Command{Use: "ntp"}
+	rootCmd := &cobra.Command{Use: _App}
 
 	rootCmd.AddCommand(cmd.NewServerCmd("server"))
 	rootCmd.AddCommand(cmd.NewClientCmd("client"))
