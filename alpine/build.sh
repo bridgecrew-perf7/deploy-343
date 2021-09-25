12846e75d739b18f9e26bbadcfab93050f90fc02
d2jvkpn <chenbin2018@protonmail.com>
2021-09-25 13:13:06 +0800

    ...

---
+++ alpine/build.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+
+_wd=$(pwd)/
+_path=$(dirname $0)/
+
+####
+name="registry.cn-shanghai.aliyuncs.com/d2jvkpn/alpine"
+tag="3"
+image=$name:$tag
+
+####
+docker pull alpine:$tag
+echo ">>> Building image: $image"
+docker build -f ./Dockerfile --no-cache -t "$image" .
+
+####
+docker push $image
+
+for img in $(docker images --filter=dangling=true --quiet $name); do
+    docker rmi $img
+done
