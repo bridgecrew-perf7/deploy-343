8227069f92161ec72d6cd8b15d42fb87374c6db3
d2jvkpn <chenbin2018@protonmail.com>
2022-05-17 16:42:32 +0800

    ...

--- productions/gitlab/minimum_requirements.sh
+++ productions/gitlab/minimum_requirements.sh
@@ -11,7 +11,7 @@ cat >> config/gitlab.rb << EOF
 ####
 puma['enable'] = true
 puma['worker_timeout'] = 120
-puma['worker_processes'] = 1
+puma['worker_processes'] = 2
 puma['per_worker_max_memory_mb'] = 1024
 
 sidekiq['max_concurrency'] = 10
@@ -23,28 +23,28 @@ gitlab_rails['env'] = {
   'GITLAB_RAILS_RACK_TIMEOUT' => 600,
 }
 
-gitaly['cgroups_count'] = 2
-gitaly['cgroups_mountpoint'] = '/sys/fs/cgroup'
-gitaly['cgroups_hierarchy_root'] = 'gitaly'
-gitaly['cgroups_memory_enabled'] = true
-gitaly['cgroups_memory_limit'] = 500000
-gitaly['cgroups_cpu_enabled'] = true
-gitaly['cgroups_cpu_shares'] = 512
+#gitaly['cgroups_count'] = 2
+#gitaly['cgroups_mountpoint'] = '/sys/fs/cgroup'
+#gitaly['cgroups_hierarchy_root'] = 'gitaly'
+#gitaly['cgroups_memory_enabled'] = true
+#gitaly['cgroups_memory_limit'] = 500000
+#gitaly['cgroups_cpu_enabled'] = true
+#gitaly['cgroups_cpu_shares'] = 512
 
-gitaly['concurrency'] = [
-  {
-    'rpc' => "/gitaly.SmartHTTPService/PostReceivePack",
-    'max_per_repo' => 3
-  }, {
-    'rpc' => "/gitaly.SSHService/SSHUploadPack",
-    'max_per_repo' => 3
-  }
-]
-gitaly['env'] = {
-  'LD_PRELOAD' => '/opt/gitlab/embedded/lib/libjemalloc.so',
-  'MALLOC_CONF' => 'dirty_decay_ms:1000,muzzy_decay_ms:1000',
-  'GITALY_COMMAND_SPAWN_MAX_PARALLEL' => '2'
-}
+#gitaly['concurrency'] = [
+#  {
+#    'rpc' => "/gitaly.SmartHTTPService/PostReceivePack",
+#    'max_per_repo' => 3
+#  }, {
+#    'rpc' => "/gitaly.SSHService/SSHUploadPack",
+#    'max_per_repo' => 3
+#  }
+#]
+#gitaly['env'] = {
+#  'LD_PRELOAD' => '/opt/gitlab/embedded/lib/libjemalloc.so',
+#  'MALLOC_CONF' => 'dirty_decay_ms:1000,muzzy_decay_ms:1000',
+#  'GITALY_COMMAND_SPAWN_MAX_PARALLEL' => '2'
+#}
 EOF
 
 docker exec gitlab_app gitlab-ctl reconfigure
