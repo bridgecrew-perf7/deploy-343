7a508f930511f08e548b91bce28fa284d2acfb4c
d2jvkpn <chenbin2018@protonmail.com>
2022-06-23 15:43:36 +0800

    ...

--- productions/prometheus/prometheus.sh
+++ productions/prometheus/prometheus.sh
@@ -14,7 +14,27 @@ docker-compose up -d
 #### reset grafana password
 docker exec -it grafana grafana-cli admin reset-admin-password PASSWORD
 
+# http://192.168.1.1:3023
+curl http://192.168.1.1:3023/metrics
+
 # grafana url: http://192.168.1.1:3022, admin PASSWORD
-# add prometheus data source: http://192.168.1.1:3023
+# add prometheus data source: http://192.168.1.1:3023/metrics
 
 ls -alh /var/lib/docker/volumes/go-web_grafana-storage/_data
+
+exit
+
+# promhttp_metric_handler_requests_total{code="200"}
+# promhttp_metric_handler_requests_total{code="200", instance="192.168.0.8:3023", job="prometheus"}
+
+process_cpu_seconds_total
+# process_cpu_seconds_total{instance="192.168.0.8:3023", job="prometheus"}
+
+go_goroutines
+# go_goroutines{instance="192.168.0.8:3023", job="prometheus"}
+
+go_gc_duration_seconds{quantile="0.75"}
+
+go_threads
+
+go_memstats_sys_bytes
