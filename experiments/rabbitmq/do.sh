4a564ffda002ffefab633419573f2c651bd910fb
d2jvkpn <chenbin2018@protonmail.com>
2022-07-26 13:41:49 +0800

    ...

--- experiments/rabbitmq/do.sh
+++ experiments/rabbitmq/do.sh
@@ -5,5 +5,7 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 docker pull rabbitmq:3-management
 
-docker run -d --name rabbitmq_app --publish=5672:5672 --publish=15672:15672 rabbitmq:3-management
+# docker run -d --name rabbitmq_app --publish=5672:5672 --publish=15672:15672 rabbitmq:3-management
+docker-compose -f docker-compose.demo.yaml up -d
+
 # http://localhost:15672, Username: guest, Password: guest
