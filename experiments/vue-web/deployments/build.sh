8241e482c3246ae43eb63332e3041e10e44e0fd2
d2jvkpn <chenbin2018@protonmail.com>
2022-09-05 13:15:12 +0800

    vue-web/deployments/build.sh tag=mode

--- experiments/vue-web/deployments/build.sh
+++ experiments/vue-web/deployments/build.sh
@@ -5,7 +5,7 @@ _path=$(dirname $0 | xargs -i readlink -f {})
 
 branch="$1" # git branch
 mode="$2"   # load .env.${mode}
-tag=$3      # image tag
+tag=$mode   # image tag
 
 #
 name="registry.cn-shanghai.aliyuncs.com/d2jvkpn/vue-web"
